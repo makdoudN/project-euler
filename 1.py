ac4a2a81d3b7d3530382371acf62e666d62a9662
makdoudN <nizam.makdoud@gmail.com>
2020-05-23 23:55:48 +0200

    First.

---
+++ 1.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+import sys
+
+
+def sum_1_to_k(n:int):
+    return (n * (n+1)) // 2
+
+def sum_1_to_k_multiple_of_p(n:int, p:int):
+    j = (n-1) // p
+    return p * sum_1_to_k(j)
+
+
+n = 100000
+sum_3 = sum_1_to_k_multiple_of_p(n, 3)
+sum_5 = sum_1_to_k_multiple_of_p(n, 5)
+sum_15 = sum_1_to_k_multiple_of_p(n, 15)
+
+result = sum_3 + sum_5 - sum_15
+
+print(result)
